29dd568f05c261dadf7d15d3d2579060ae85e8c8
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-11-24 05:13:28 +0530

    add todo

--- main.go
+++ main.go
@@ -7,12 +7,40 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID int `json: "id"`
+	Completed bool `json: "completed"`
+	Body string `json: "body"` 
+}
+
 func main() {
 	fmt.Println("Hello world")
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error { // Ctx for context
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	// create a todo
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // {id:0, completed: false, body: ""}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		fmt.Print(c)
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":4000"))
