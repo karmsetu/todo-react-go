aa11e0cb46a0a39a39bf5b04dba0ed063c3b63ff
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-11-24 05:14:47 +0530

    update todo - set completed field to true

--- main.go
+++ main.go
@@ -43,5 +43,19 @@ func main() {
 		return c.Status(201).JSON(todo)
 	})
 
+
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
