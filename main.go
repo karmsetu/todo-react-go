086f6ccc4f448e5b464781659a8a747ae8ecd15b
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-11-24 05:22:32 +0530

    delete and update all todo

--- main.go
+++ main.go
@@ -23,6 +23,10 @@ func main() {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
 	})
 
+	app.Get("/api/todos", func(c *fiber.Ctx) error { // Ctx for context
+		return c.Status(200).JSON(todos)
+	})
+
 	// create a todo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{} // {id:0, completed: false, body: ""}
@@ -54,6 +58,19 @@ func main() {
 			}
 		}
 
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
+	})
+
+
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]... )
+				return c.Status(200).JSON(fiber.Map{"success": "true"})
+			}
+		}
 		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
 	})
 
